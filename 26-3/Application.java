31d9d850bc7c1325a48125ae7514e72508d02c90
TruongDDTH2107028 <88825108+TruongDDTH2107028@users.noreply.github.com>
2022-03-26 13:36:20 +0700

    26-3
    update

---
+++ 26-3/Application.java
@@ -0,0 +1,8 @@
+package atmMachine;
+
+public class Application {
+    public static void main(String[] args) throws Exception {
+        Controller controller = new Controller();
+        controller.home();
+    }
+}
