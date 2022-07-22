7c7c69ef3a221865426d9168344d49128cee601f
TruongDDTH2107028 <88825108+TruongDDTH2107028@users.noreply.github.com>
2022-07-22 23:05:08 +0700

    StudentManagement
    update

---
+++ 17-7/config.php
@@ -0,0 +1,10 @@
+<?php
+     $dataHost ='localhost';
+     $dataName ='stu_crud';
+     $dataUserName ='root';
+     $dataPassword ='';
+
+     $mysqli = mysqli_connect($dataHost,$dataUserName,$dataUserName,$dataPassword);
+
+?>
+
