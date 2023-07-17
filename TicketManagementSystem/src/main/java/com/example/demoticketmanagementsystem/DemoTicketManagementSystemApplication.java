a2a18632d9a99712bd0ee03fbf64e1d5ac109801
TruongDDTH2107028 <88825108+TruongDDTH2107028@users.noreply.github.com>
2023-07-17 21:48:45 +0700

    management
    update

---
+++ TicketManagementSystem/src/main/java/com/example/demoticketmanagementsystem/DemoTicketManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoticketmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTicketManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoTicketManagementSystemApplication.class, args);
+	}
+
+}
---
+++ TicketManagementSystem/src/test/java/com/example/demoticketmanagementsystem/DemoTicketManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoticketmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTicketManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
